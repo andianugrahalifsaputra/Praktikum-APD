005d7ea88e81569c82cade5842c161d20bb7cbb9
andianugrahalifsaputra <andianugrahalifsaputraa@gmail.com>
2024-10-04 15:11:06 +0800

    Pertemuan 5

---
+++ Praktikum APD/Kelas/Pertemuan ke-5/CRUD.py
@@ -0,0 +1,52 @@
+# data = [1,2,3,4,[1,2]]
+# # print(type(data))
+# print(data[-1][-1])
+
+# nilai = [1,3,5,7]
+# print(nilai)
+# print()
+# nilai.append(11)
+# print(nilai)
+# nilai.insert(0, 0)
+# print()
+# print(nilai)
+# nilai.extend([13, 17])
+# print()
+# print(nilai)
+# print()
+
+# #Mengubah list
+# nilai[0] : "Nol"
+# print(nilai)
+# print()
+
+# #tidak bisa dimasukkan ke dalam variabel
+# del nilai[1]
+# print(nilai)
+# print()
+
+# #bisa dimasukkan ke dalam variabel
+# nilai.pop(2)
+# print(nilai)
+
+# data_mahasiswa =[
+#     ["Admin", "Admin12", "admin"]
+#     ["User", "User12", "user"]
+# ]
+
+# hewan = ["Gajah", "Semut", "Kucing"]
+#01
+# for data in  hewan:
+#     print(data)
+
+# #02
+# for index, item in enumerate[hewan]:
+#     print(f"data ke-{index + 1}")
+    
+#Tuple
+# hewan1, hewan2, hewan3 = hewan
+# print(hewan1)
+# print(hewan2)
+# hewan1, *banyak_hewan = hewan
+# print(hewan)
+# print(banyak_hewan)
