ae2c3fb859c187e47274250b0ddaf904f24c2bd3
andianugrahalifsaputra <andianugrahalifsaputraa@gmail.com>
2024-10-18 14:59:36 +0800

    Pertemuan ke-7

---
+++ Praktikum APD/Kelas/Pertemuan ke-9/fungsi.py
@@ -0,0 +1,24 @@
+# nama = "Dimas"
+# def say_hello():
+#     nama="Alang"
+#     print(nama, "dalam fungsi")
+# print(nama, "luar fungsi")
+# say_hello()
+
+# def luas_persegi_p(panjang, lebar):
+#     luas = panjang*lebar
+#     print(f"Luas persegi panjang adalah{luas}")
+# print(10, 18)
+
+# def faktorial(n):
+#     if n == 1:
+#         return 1
+#     else:
+#         return n*faktorial(n-1)
+# print(faktorial(7))
+
+
+def say_hello():
+    nama = print(f"Masukkan nama anda:{nama}")
+    return nama
+print("andi")
